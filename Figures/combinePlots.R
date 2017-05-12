aacfeb7dd9bba146bb0bfbc141085b50f916d097
hakurban <hakurban@indiana.edu>
2017-05-11 20:31:13 -0400

    updateing Figures folder

--- Figures/combinePlots.R
+++ Figures/combinePlots.R
@@ -1,5 +1,9 @@
+#Combining figures
+
 require(grid)
 require(gridExtra)
+
+
 #grid.arrange(n1,k1,d1,n3,k3,d3,ncol = 3,top=textGrob("Experimental Results for Multivarite Synthetic Data Sets", gp=gpar(fontsize=15,font=8)))
 
 grid.arrange(cancer3,cancer1,cancer2,census3,census1,census2,census103,census101,census102,ncol = 3)
